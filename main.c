bdaee5b32f69673dd7c46eb0f556eda3d309e806
MaxiRamos <maxiramos@outloo.com>
2014-02-17 11:27:03 -0300

    Ultimo
    a

--- main.c
+++ main.c
@@ -14,22 +14,22 @@ void abrirArchivo(FILE *pf,char* nombre, char*modo);
 
 int main()
 {   srand(time(NULL));
-    FILE archivoBinario;
-    char nombres[]={"AB","BE","CE","DE","PEPE","PEPITO","PEPAZO","BOCA","RIVER",
+    FILE *archivoBinario;
+    char *nombres[]={"AB","BE","CE","DE","PEPE","PEPITO","PEPAZO","BOCA","RIVER",
                     "SAN LORENZO","ALBERT","TESLA","JACK OUBREY","YO","TU","EL",
                     "NOSOTROS","VOSOTROS","ELLOS",
                     "USTED","MIO","TUYO","FACTURAS","CON","CREMA"}; //el numero de agrupacion va del 0 al 24
-    abrirArchivo(pf,"agrupacioes.bin","wb");
-    if(pf==NULL)return 0;
+    abrirArchivo(archivoBinario,"agrupaciones.bin","wb");
+    if(archivoBinario==NULL)return 0;
     int i=0;
     int num=0;
     t_datoSimple reg;
     while(i<=MAX){
         num=rand()%24;
 
-        reg.nombre=nombres[num];
+        reg.nombreAgrup=nombres[num];
         reg.nAgrup=num;
-        fwrite(&reg,sizeof(t_datoSimple),1,pf);
+        fwrite(&reg,sizeof(t_datoSimple),1,archivoBinario);
 
     }
     printf("Hello world!\n");
